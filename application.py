9858e62e10e4953c3690886f68cc199edbc4eb2d
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-05-03 00:48:55 +0530

    done with app creation

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import joblib 
-import numpy
+import numpy as np
 from config.path_config import MODEL_OUTPUT_PATH
 from flask import Flask, render_template, request
 
@@ -11,12 +11,26 @@ def index():
     if request.method=="POST":
         lead_time = int(request.form["lead_time"])
         no_of_special_request = int(request.form["no_of_special_request"])
-        avg_price_per_room = request.form["avg_price_per_room"]
+        avg_price_per_room = float(request.form["avg_price_per_room"])
         arival_month = int(request.form["arival_month"])
         arival_date = int(request.form["arival_date"])
-        
-        
 
-       
+        market_segment_type = int(request.form["market_segment_type"])
+        no_of_week_nights = int(request.form["no_of_week_nights"])
+        no_of_weekend_nights = int(request.form["no_of_weekend_nights"])
 
+        type_of_meal_plan = int(request.form["type_of_meal_plan"])
+        room_type_reserved = int(request.form["room_type_reserved"])
+
+        features = np.array([[lead_time, no_of_special_request, avg_price_per_room, arival_month, arival_date, market_segment_type, no_of_week_nights, no_of_weekend_nights, type_of_meal_plan, room_type_reserved]])
+
+        prediction = model.predict(features)
+
+        return render_template('index.html', prediction = prediction[0])
+    return render_template('index.html', prediction=None)
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=5100)
+        
 
